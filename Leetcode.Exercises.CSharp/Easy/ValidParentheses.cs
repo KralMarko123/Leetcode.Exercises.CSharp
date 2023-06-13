a9481d39bed74a2b206d31cf08007db2b5abae66
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-06-13 16:15:11 +0200

    added validpalindrome & validparentheses

---
+++ Leetcode.Exercises.CSharp/Easy/ValidParentheses.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Leetcode.Exercises.CSharp.Easy
+{
+    public static class ValidParentheses
+    {
+        // Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+        // An input string is valid if:
+        // Open brackets must be closed by the same type of brackets.
+        // Open brackets must be closed in the correct order.
+        // Every close bracket has a corresponding open bracket of the same type.
+
+        public static bool IsValid(string s)
+        {
+            if (s.Length == 1) return false;
+
+            var stack = new Stack<char>();
+
+            foreach (var character in s)
+            {
+                if (character == '(') { stack.Push(')'); continue; }
+                if (character == '{') { stack.Push('}'); continue; }
+                if (character == '[') { stack.Push(']'); continue; }
+                if (stack.Count == 0 || character != stack.Pop()) return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
