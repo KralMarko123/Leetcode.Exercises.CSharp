309846a1407d3898c80ee90ce3868cba91dc5f55
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2023-06-19 14:42:34 +0200

    added longest consecutive sequence

---
+++ Leetcode.Exercises.CSharp/Medium/LongestConsecutiveSequence.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Leetcode.Exercises.CSharp.Medium
+{
+    // Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+    // You must write an algorithm that runs in O(n) time.
+    public static class LongestConsecutiveSequence
+    {
+        public static int LongestConsecutive(int[] nums)
+        {
+            if (nums.Length == 0 || nums.Length == 1) return nums.Length;
+
+            // hashset lookup is O(1)
+            var set = nums.ToHashSet();
+            var maxSequence = 1;
+            var sequence = 0;
+
+            foreach (var num in nums)
+            {
+                if (set.Contains(num - 1)) continue;
+
+                sequence = 1;
+
+                // since lookup is O(1) worst case scenario is an interrupted sequence which would result in O(N)
+                while (set.Contains(num + sequence)) sequence++;
+
+                maxSequence = maxSequence < sequence ? sequence : maxSequence;
+            }
+
+            return maxSequence;
+        }
+    }
+}
