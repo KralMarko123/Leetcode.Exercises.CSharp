c690fdee3db687598ce88e437851820fa795eb9d
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2024-02-06 16:36:42 +0100

    .

--- Leetcode.Exercises.CSharp/Easy/FindTheIndexOfTheFirstOccurenceInAString.cs
+++ Leetcode.Exercises.CSharp/Easy/FindTheIndexOfTheFirstOccurenceInAString.cs
@@ -6,9 +6,29 @@ namespace Leetcode.Exercises.CSharp.Easy
     //or -1 if needle is not part of haystack.
     public static class FindTheIndexOfTheFirstOccurenceInAString
     {
-        public static int StrStr(string haystack, string needle)
+        public static int FindIndexOfFirstOccurence(string haystack, string needle)
         {
-            return haystack.IndexOf(needle);
+            if (needle.Length > haystack.Length) return -1;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                if (haystack.Length - i < needle.Length) return -1;
+
+                if (haystack[i] == needle[0])
+                {
+                    var lettersPassed = 0;
+
+                    for (int j = i; lettersPassed < needle.Length; j++)
+                    {
+                        if (haystack[j] == needle[lettersPassed]) lettersPassed++;
+                        else break;
+                    }
+
+                    if (lettersPassed == needle.Length) return i;
+                }
+            }
+            
+            return -1;
         }
     }
 }
--- Leetcode.Exercises.CSharp/Program.cs
+++ Leetcode.Exercises.CSharp/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine();
+﻿using Leetcode.Exercises.CSharp.Easy;
+using Leetcode.Exercises.CSharp.Medium;
+//using Leetcode.Exercises.CSharp.Hard;
+
+Console.WriteLine();
+
+
+
