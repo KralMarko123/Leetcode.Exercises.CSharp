205c625e9fb8cc4ac842429e3fa81e5042811509
MarkoMarkovikjEndava <marko.markovikj@endava.com>
2024-02-09 16:09:11 +0100

    .

--- Leetcode.Exercises.CSharp/Medium/RemoveNthNodeFromEndOfList.cs
+++ Leetcode.Exercises.CSharp/Medium/RemoveNthNodeFromEndOfList.cs
@@ -5,10 +5,9 @@ namespace Leetcode.Exercises.CSharp.Medium
     // Given the head of a linked list, remove the nth node from the end of the list and return its head.
     public static class RemoveNthNodeFromEndOfList
     {
-        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        public static ListNode? RemoveNthFromEnd(ListNode head, int n)
         {
-            int length = 0;
-            int pos = 1;
+            int length = 0, pos = 1;
             ListNode p = head;
 
             while (p != null)
